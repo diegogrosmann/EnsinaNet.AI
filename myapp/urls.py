4fbb558f55c4f22d0c4b4b954e80b1390ae6156f
Diego Grosmann <diegogrosmann@bol.com.br>
2024-08-18 16:56:28 -0300

    .eve

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     path('', views.hello_world, name='index'),
     path('api/compare_lab/', views.compare_lab, name='compare_lab'),
     path('api/compare_instrucao/', views.compare_instrucao, name='compare_instrucao')
-]
+] 
